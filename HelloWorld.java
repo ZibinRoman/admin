bc0a4619647b8abe31f5e92e0821bf5f9b8901f7
ZibinRoman <61138127+ZibinRoman@users.noreply.github.com>
2020-02-27 13:55:53 +0500

    Zibin roman patch 1 (#2)
    * Update HelloWorld.java
    
    * Create README.md
    
    * 77
    
    * 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,8 +14,10 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("18130166 ", d.width, d.height, g);
-g.drawRect(0,0,d.width-1,d.height-1); 
+
+drawCenteredString("18130166", d.width, d.height, g);
+g.drawRect(0,0,d.width-1,d.height-1);
+
 }
 
 public void drawCenteredString(String s, int w, int h, Graphics g) { 
